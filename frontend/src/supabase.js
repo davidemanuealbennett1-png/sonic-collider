8cd596db888f57e4ca64b8fc07848026a75e6606
David Bennett <davidemanuealbennett@gmail.com>
2026-03-15 23:41:01 +0200

    fix supabase key

--- frontend/src/supabase.js
+++ frontend/src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://fnqvotibheiadhsrmloz.supabase.co'
-const supabaseKey = 'sb_publishable_TDw4uSE4engFSsbcIJMp8Q_wFTjKuYd'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZucXZvdGliaGVpYWRoc3JtbG96Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM1MDg0MDcsImV4cCI6MjA4OTA4NDQwN30.d1NfWni7dxgKsS4OPM-zAjJbRKb-aOwBvo_YJVX9Lhs'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
